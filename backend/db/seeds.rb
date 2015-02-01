e037a46abe75288c1b132d4902f900a2f1e271b2
Helen Hood <helen.c.hood@gmail.com>
2015-02-01 16:07:03 -0500

    Add seeder for projects

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+project_attrs = [
+  {
+    name: "Build first ember app",
+    description: "Build an ember app so I can be a sweet Ember developer and make mad $$$$$"
+  },
+  {
+    name: "Troll Eric",
+    description: "Formulate plans to continuously troll Eric because it is fun."
+  },
+  {
+    name: "Hack Richard's OK Cupid",
+    description: "Tryna mess up Richard's dating game"
+  }
+]
+
+project_attrs.each do |attrs|
+  project = Project.find_by(attrs)
+
+  if project.present?
+    puts "Project '#{attrs[:name]}' already in DB."
+  else
+    Project.create(attrs)
+    puts "Project '#{attrs[:name]}' created."
+  end
+end
